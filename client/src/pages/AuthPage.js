c502a86922dba0fdf8d04ab95ced6a603a86100e
Vsevolod Berdutin <vesevolodberdutin@gmail.com>
2021-06-12 21:26:51 +0300

    authPage component_5

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const AuthPage = () => {
-    return (
-        <div>
-            <h1>Auth Page</h1>
+    const [form, setForm] = useState({
+        email:'', password:''
+    })
+
+    const changeHandler = event => {
+        setForm({...form, [event.target.name]:event.target.value})
+    }
+
+  return (
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h1>Make the link shorter</h1>
+
+        <div className="card blue darken-1">
+          <div className="card-content white-text">
+            <span className="card-title">Authorizatin</span>
+            <div>
+
+            <div className="input-field">
+                <input 
+                    placeholder="Input email" 
+                    id="email" 
+                    type="text"
+                    name="email"
+                    className="yellow-input"
+                    onChange={changeHandler}
+                />
+                <label htmlFor="email">Email</label>
+            </div>
+
+            <div className="input-field">
+                <input 
+                    placeholder="Input password" 
+                    id="password" 
+                    type="password"
+                    name="password"
+                    className="yellow-input"
+                    onChange={changeHandler}
+                />
+                <label htmlFor="password">Password</label>
+            </div>
+
+            </div>
+          </div>
+          <div className="card-action">
+              <button className="btn yellow darken-4" style={{marginRight: 10}}>Sign in</button>
+              <button className="btn grey lighten-1 black-text">Sign up</button>
+            
+          </div>
         </div>
-    )
+
+      </div>
+    </div>
+  )
 }
