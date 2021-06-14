6f11ddeb86d2deac7cbfa2470eb91154a903fb2a
Vsevolod Berdutin <vesevolodberdutin@gmail.com>
2021-06-14 12:11:37 +0300

    link card

---
+++ client/src/components/LinkCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const LinkCard = ({link}) => {
+    return (
+        <>
+        <h2>Link</h2>
+
+        <p>Your link: <a href={link.to} target="_blank" rel="noopener noreferrer">{link.to}</a></p>
+        <p>Wrom where: <a href={link.from} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
+        <p>Number of clicks by link: <strong>{link.clicks}</strong></p>
+        <p>Date of creating: <strong>{new Date(link.date).toLocaleDateString()}</strong></p>
+        </>
+    )
+
+}
